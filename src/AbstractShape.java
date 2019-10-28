c8039ca79bb8278af2e20677bb755fc76af898f7
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-10-28 12:12:08 -0700

    Moved x, y, and c to AbstractShape
    These fields are shared by both concrete shapes, so putting them here cleans a bit the redundancies.

--- src/AbstractShape.java
+++ src/AbstractShape.java
@@ -1,4 +1,7 @@
 
 public abstract class AbstractShape implements Shape{
+	protected int x;
+	protected int y;
+	protected Color c;
 	// I think abstract class is used for draw Hshape and FibonacciTiles.
 }
